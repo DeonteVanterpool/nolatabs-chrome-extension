83e77ff3d0f8a275e72cece5b32608c7142cfd18
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2025-09-06 16:03:39 -0400

    changed command pallete shortcut, and removed popup from manifest

--- src/pages/Background/commands.ts
+++ src/pages/Background/commands.ts
@@ -1,7 +1,11 @@
+import {openWelcomePage} from "./services";
+
 chrome.commands.onCommand.addListener((command) => {
-    if (command === "command-pallete")
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id as number, { action: "showCommandPallete" });
-    });
+    if (command === "command-pallete") {
+        chrome.tabs.query({index: 0}, (tab) => {
+            let id: number = tab[0].id!;
+            chrome.tabs.update(id, {active: true});
+        });
+    }
 });
 
