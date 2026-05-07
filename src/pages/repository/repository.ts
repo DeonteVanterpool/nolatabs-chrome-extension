7b4e0625643f55527e9a90fbbf62107e33fc5e3a
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2026-05-06 22:45:04 -0400

    added more functional style to repositories and nested the repositories object further

--- src/pages/repository/repository.ts
+++ src/pages/repository/repository.ts
@@ -1,84 +1,75 @@
 import {Repository} from '../models/repository';
 import {CommitStore} from './commit';
-import {StorageDTO} from './store';
 
-type RepositoryStorageItem = RepositoryStorageItemV1; // add future versions here using union types
-type RepositoryStorage = RepositoryStorageV1;
+type RepositoryDTO = RepositoryDTOV1; // add future versions here using union types
+type RepositoryContainer = RepositoryContainerV1; // this is the object that will be stored in chrome.storage. Never build repository collection objects directly, always use the serialization functions
 export const LATEST_VERSION = 1;
 
-type RepositoryStorageItemV1 = {
+type RepositoryDTOV1 = {
     name: string,
     owner: string,
     version: number,
 }
 
-type RepositoryStorageV1 = {
-    repositories: RepositoryStorageItem[];
-    version: number,
+type RepositoryContainerV1 = {
+    items: RepositoryDTO[];
+    oldestVersion: number, // this is the version of the oldest repository in the repositories array. This is used to determine if migrations need to be run when deserializing the repositories
 }
 
-class RepositoryDTO extends StorageDTO<Repository[], RepositoryStorage> {
-    deserialize(repositories: RepositoryStorage): Repository[] {
-        if (repositories.version < LATEST_VERSION) {
-            // run migrations
-        }
-        return repositories.repositories.map((obj) => {
-            return {
-                name: obj.name, owner: obj.owner
-            }
-        });
+// This is the repository item that will be stored in chrome.storage. Never build repository objects directly, always use the serialization functions
+function deserializeRepo(repositories: RepositoryContainer): Repository[] {
+    if (repositories.oldestVersion < LATEST_VERSION) {
+        // run migrations
     }
-
-    serialize(models: Repository[]): RepositoryStorage {
+    return repositories.items.map((obj) => {
         return {
-            repositories: models.map((obj) => {
-                return {
-                    name: obj.name,
-                    owner: obj.owner,
-                    version: LATEST_VERSION,
-                };
-            }),
-            version: LATEST_VERSION
-        };
-    }
+            name: obj.name, owner: obj.owner
+        }
+    });
+}
 
-    public constructor() {
-        super();
-    }
+// This is the repository item that will be stored in chrome.storage. Never build repository objects directly, always use the serialization functions
+function serializeRepo(models: Repository[]): RepositoryContainer {
+    return {
+        items: models.map((obj) => {
+            return {
+                name: obj.name,
+                owner: obj.owner,
+                version: LATEST_VERSION,
+            };
+        }),
+        oldestVersion: LATEST_VERSION
+    };
 }
 
 export class RepositoryStore {
-    storage: chrome.storage.StorageArea;
-
-    public constructor(storage: chrome.storage.StorageArea) {
-        this.storage = storage;
+    public static async initialized(storage: chrome.storage.StorageArea): Promise<boolean> {
+        return Object.keys(await storage.get("repositories")).length !== 0;
     }
 
-    public async initialized() {
-        return Object.keys(await this.storage.get("repositories")).length !== 0;
-    }
-
-    public async init() {
-        if (await this.initialized()) {
+    /** Initializes the repositories storage with an empty array. Throws an error if the repositories storage is already initialized. */
+    public static async init(storage: chrome.storage.StorageArea) {
+        if (await RepositoryStore.initialized(storage)) {
             throw new Error("Repositories storage already initialized");
         }
-        await this.storage.set({repositories: []});
+        await storage.set({repositories: serializeRepo([])});
     }
 
-    public async read(): Promise<Repository[]> {
-        return new RepositoryDTO().deserialize((await this.storage.get("repositories")) as RepositoryStorage);
+    public static async read(storage: chrome.storage.StorageArea): Promise<Repository[]> {
+        let data = await storage.get("repositories");
+        if (!data.repositories) {
+            throw new Error("Repositories storage not initialized");
+        }
+        return deserializeRepo(data.repositories as RepositoryContainer);
     }
 
-    public async create(repo: Repository) {
-        let commitRepo = new CommitStore(this.storage);
-        await commitRepo.init(repo);
-        await this.storage.set(
-            new RepositoryDTO().serialize([...await this.read(), repo]));
+    public static async create(storage: chrome.storage.StorageArea, repo: Repository) {
+        await CommitStore.init(storage, repo);
+        await storage.set({repositories: serializeRepo([...await RepositoryStore.read(storage), repo])});
     }
 
-    public async delete(repo: Repository) {
-        await this.storage.set(
-            new RepositoryDTO().serialize((await this.read()).filter((r) => !(r.name === repo.name && r.owner === repo.owner))));
+    public static async delete(storage: chrome.storage.StorageArea, repo: Repository) {
+        await storage.set({repositories: serializeRepo((await RepositoryStore.read(storage)).filter((r) => !(r.name === repo.name && r.owner === repo.owner)))});
     }
 }
 
